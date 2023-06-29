b08a47f974e4055e8d63a5a3b3dcdf96432dd350
Cristian Gonzalez-Colin <cristianj_gc@hotmail.com>
2023-06-29 12:32:05 -0700

    Update pbs_submit.sh

--- pbs_submit.sh
+++ pbs_submit.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-#SBATCH --job-name=multiple_eQTL
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=10g
-#SBATCH --time=120:00:00
-#SBATCH --output=/path/to/sdoutput/pcs.out
-#SBATCH --error=/path/to/sderr/pcs.err
-
-cd /path/to/pipeline
-
-WORKDIR=/path/workingdirectory/eQTL_pipeline/
-log_path=${WORKDIR}/logs/
-
-start=`date +%s`
-date
+#PBS -N WGS_snakemake
+#PBS -o /path/to/pipeline/WGS_snakemake.out
+#PBS -e /path/to/pipeline/WGS_snakemake.err
+#PBS -q default
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=10gb
+#PBS -l walltime=120:00:00
 
 
-snakemake --jobs 100 --latency-wait 60 --snakefile Snakefile --configfile snake_conf.yaml --cluster-config cluster.json --cluster "sbatch --time={cluster.walltime} --nodes=1 --ntasks=1 --cpus-per-task=4 --mem={cluster.memory} -e /${log_path}/{rule}.{jobid}.{wildcards}.err -o /${log_path}/{rule}.{jobid}.{wildcards}.out --export ALL --parsable" --stats $log_path/snakemake.stats >& $log_path/snakemake.log  --rerun-incomplete --use-conda --cluster-status smk-slurm.sh 
+### Example on a PBS torque system
 
-end=`date +%s`
-runtime=$((end-start))
-echo 'Running time ' ${runtime} ' seconds'
+
+code_path=/path/to/wgs/pipeline/
+log_path=$code_path/logs
+
+cd ${code_path}
+mkdir -p ${log_path}
+
+
+snakemake --jobs 150 --latency-wait 600 --cluster-config cluster.json --cluster "qsub -l {cluster.walltime} -l {cluster.cores} -l {cluster.memory} -m n -q default -e $log_path/{cluster.error} -o $log_path/{cluster.output}" --jobname 's.{rulename}.{jobid}' --stats $log_path/snakemake.stats >& $log_path/snakemake.log 
